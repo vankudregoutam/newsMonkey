6762f144e31460a3179ad3677ea22505e348ecd1
unknown <vankudregoutam15@gmail.com>
2023-10-08 16:05:41 +0530

    final commit

--- src/components/News.js
+++ src/components/News.js
@@ -24,7 +24,7 @@ const News = (props) => {
         let parsedData = await data.json();
         props.setProgress(70)
         setArticles(parsedData.articles)
-        setTotalResults(parsedData.totalResluts)
+        setTotalResults(parsedData.totalResults)
         setLoading(false)
         props.setProgress(100)
     }
